e83d2ea15d56d7866ce9447d692d3b271a552705
HP <HP@DESKTOP-POHI4N8>
2022-02-20 14:18:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Delta
+{
+    class Star
+    {
+        static void Main(string[] args) 
+        {
+            int i;
+            Console.WriteLine("Star printing");
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine();
+                for (int j = 1; j <= i + 1; j++)
+
+                    Console.Write("*");
+            }
+                Console.ReadLine();
+
+
+
+        }
+    }
+}
